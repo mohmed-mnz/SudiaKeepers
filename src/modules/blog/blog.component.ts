63c2430e339906fa8b00aeb11cd8627bd4b88c80
Alaa Mohey <am5430@fayoum.edu.eg>
2024-08-05 01:51:48 +0300

    changes

--- src/modules/blog/blog.component.ts
+++ src/modules/blog/blog.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-
+import { RouterOutlet } from '@angular/router';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
@@ -14,7 +14,8 @@ import { InputTextModule } from 'primeng/inputtext';
     CommonModule,
     InputTextModule,
     InputIconModule,
-    IconFieldModule
+    IconFieldModule,
+    RouterOutlet
   ],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
@@ -22,13 +23,13 @@ import { InputTextModule } from 'primeng/inputtext';
 export class BlogComponent {
   constructor(private router: Router) { }
   sections = [
-    { id: 'section1', title: 'تنسق الحدائق', itemImageSrc: '../../assets/p5.jpg',url:'/agriculturalDecoration',
+    { id: 'section1', title: 'تنسق الحدائق', itemImageSrc: 'assets/5864118342326010007.jpg',url:'/agriculturalDecoration',
       content: ' يحتاج الكثير من الأشخاص إلى الاسترخاء والهدوء والراحة النفسية بعد عناء وتعب يومٍ طويل في عملهم، ولن يتحقق ذلك إلا بالجلوس بين أحضان الطبيعة؛ حيث الزروع الخضراء والأشجار الرائعة، والمياه الجارية والورود الأنيقة، والمساحات الخضراء الغنّاء، ويمكن تحقيق ذلك داخل حديقتك.   ' },
-    { id: 'section2', title: 'زرع صناعى', itemImageSrc: '../../assets/gn1.jpg',url:'/unnatural-grass',
+    { id: 'section2', title: 'زرع صناعى', itemImageSrc: 'assets/5864118342326010026.jpg',url:'/unnatural-grass',
       content: 'يُعتبر الزرع الصناعي بمختلف أنواعه سواء كان شجر صناعي أو ورود صناعية من أهم الأشياء التي يتم الاعتماد عليها بشكل كبير في تزيين المنازل والفلل والقصور، وخاصًة في الآونة الأخيرة بسبب كثرة ألوانه وأنواعه المختلفة، ولهذا سوف نخصص حديثنا عن تعريفه، وأنواعه، وأسعاره، وأفضل شركة توفر زرع صناعي في المملكة العربية السعودية.   ' },
-    { id: 'section3', title: 'تصميم وتركيب نوافير', itemImageSrc: '../../assets/W1.jpg',url:'/fountains',
+    { id: 'section3', title: 'تصميم وتركيب نوافير', itemImageSrc: 'assets/5843704126544922503.jpg',url:'/fountains',
        content: ' يحتاج الكثير من الأشخاص إلى الاسترخاء والهدوء والراحة النفسية بعد عناء وتعب يومٍ طويل في عملهم، ولن يتحقق ذلك إلا بالجلوس بين أحضان الطبيعة؛ حيث الزروع الخضراء والأشجار الرائعة، والمياه الجارية والورود الأنيقة، والمساحات الخضراء الغنّاء، ويمكن تحقيق ذلك داخل حديقتك      ' },
-    { id: 'section4', title: 'تصميم وتركيب شلالات', itemImageSrc: '../../assets/S1.jpg',url:'/Waterfall',
+    { id: 'section4', title: 'تصميم وتركيب شلالات', itemImageSrc: 'assets/5843704126544922508.jpg',url:'/Waterfall',
        content: ' إحدى أهم الأمور المستحدثة والتي تستخدم في تزيين الحدائق وبعض القصور والفلل، وتساهم في إعطاء مناظر طبيعية خلابة ساحرة تجذب الرائي، وللتعرف عليها وعلى أنواعها وعلى ' }
   ];
 
@@ -45,6 +46,7 @@ export class BlogComponent {
   }
 
   openNewTab(url:any){
-    window.open(this.router.serializeUrl(this.router.createUrlTree([url])), '_blank');
+    this.router.navigate([url])
+   
   }
 }
